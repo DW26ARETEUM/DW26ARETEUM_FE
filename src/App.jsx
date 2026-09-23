de487b41cac0e8c3aa1e5ff61b4be9f487d260af
swifturtle <bliss3361@gmail.com>
2026-09-24 04:00:14 +0900

    화면 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-  return <div className="app-shell" />;
+  return (
+    <div className="pc-background">
+      <div className="mobile-frame">
+        <div className="mobile-content">
+          <div className="app-content"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
