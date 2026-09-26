ea16d3409156867b048be0064f25ef8c61c44b4f
ChaeYeon-J <jjso0467@gmail.com>
2026-09-26 15:35:10 +0900

    feat: 플리마켓 부스 상세 페이지 라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import "./styles/App.css";
+import { Route, Routes } from "react-router-dom";
 import PerformanceTimetable from "./pages/PerformanceTimetable.jsx";
 import PerformanceDetail from "./pages/PerformanceDetail.jsx";
 import FoodTruckDetail from "./pages/FoodTruckDetail.jsx";
+import SomCollectionDetail from "./pages/SomCollectionDetail.jsx";
 
 function App() {
   return (
@@ -9,7 +11,13 @@ function App() {
       <div className="mobile-frame">
         <div className="mobile-content">
           <div className="app-content">
-            <FoodTruckDetail />
+            <Routes>
+              <Route path="/" element={<FoodTruckDetail />} />
+              <Route
+                path="/booth/som-collection/:day/:boothId"
+                element={<SomCollectionDetail />}
+              />
+            </Routes>
           </div>
         </div>
       </div>
